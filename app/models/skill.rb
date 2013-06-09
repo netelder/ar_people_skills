5b0ac9f57d39325ff6fdc8a4480cef0c7e910e0f
Lloyd Taylor <ltaylor@netelder.com>
2013-06-09 07:38:50 -0700

    completed User class

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,4 +2,9 @@ class Skill < ActiveRecord::Base
   has_many :user_skills
   has_many :users, :through => :user_skills
   # Remember to create a migration!
+  validates :name, :uniqueness => :true
+
+  def user_with_proficiency(proficiency)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ActiveRecord::Base
   # Remember to create a migration!
   has_many :user_skills
   has_many :skills, :through => :user_skills
+
+  def set_proficiency_for(skill_name, proficiency)
+  	skill_id = Skill.find_by_name(skill_name).id
+  	userskill_id = UserSkill.where(:user_id => self.id, :skill_id => 
+  		skill_id)
+  	UserSkill.update(userskill_id, :proficiency => proficiency)
+  end
+
+  def proficiency_for(skill_name)
+  	skill_id = Skill.find_by_name(skill_name).id
+  	UserSkill.where(:user_id => self.id, :skill_id => skill_id).first.proficiency
+  end
+
 end
